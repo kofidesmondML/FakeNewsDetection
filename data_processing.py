4ca93a2614e48cff7ea28a7d2a1fa56e152bf8fd
Desmond Kofi Boateng <desmondkofiboateng@GRAD-PHDCOMP03.local>
2024-10-25 10:17:16 -0600

    Converted all json files into csvs

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,6 @@
 import pandas as pd 
-
+import os
+import json
 file_path = './PolitiFact/PolitiFactNewsUser.txt'
 
 with open(file_path, 'r') as file:
@@ -24,3 +25,23 @@ df=pd.read_csv(user_user_path,delimiter='\t', names=column_names)
 print(df.head())
 
 df.to_csv('./data/User_User.csv', index=False)
+
+
+def json_folder_to_csv(folder_path, output_csv):
+    data = []
+    all_keys = set()
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.json'):
+            file_path = os.path.join(folder_path, filename)
+            with open(file_path, 'r') as f:
+                json_data = json.load(f)
+                all_keys.update(json_data.keys())
+                data.append(json_data)
+
+    df = pd.DataFrame(data)
+    df = df.reindex(columns=all_keys)
+    df.to_csv(output_csv, index=True)
+    print(f"Data successfully saved to {output_csv}")
+
+json_folder_to_csv('./PolitiFact/FakeNewsContent/', './data/FakeNewsContent.csv')
+json_folder_to_csv('./PolitiFact/RealNewsContent/','./data/RealNewsContent.csv')
